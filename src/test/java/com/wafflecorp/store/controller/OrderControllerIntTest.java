a6c77457de140d1e32820d560e80d78316d29c2d
Dan Vega <danvega@gmail.com>
2022-04-13 15:08:02 -0400

    Adding Order Controller integration test as an example of using HttpGraphQlTester

---
+++ src/test/java/com/wafflecorp/store/controller/OrderControllerIntTest.java
@@ -0,0 +1,57 @@
+package com.wafflecorp.store.controller;
+
+import com.wafflecorp.store.model.Order;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * An example of using HttpGraphQlTester
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OrderControllerIntTest {
+
+    private HttpGraphQlTester graphQlTester;
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        WebTestClient client = WebTestClient.bindToServer()
+                .baseUrl(String.format("http://localhost:%s/graphql", port))
+                .build();
+
+        graphQlTester = HttpGraphQlTester.create(client);
+    }
+
+    @Test
+    void contextLoads() {
+        assertNotNull(graphQlTester);
+    }
+
+    @Test
+    void shouldRespondWithAllOrders() {
+        String document = """
+            query {
+                allOrders {
+                    id
+                    qty
+                    status
+                }
+            }        
+        """;
+
+        graphQlTester.document(document)
+                .execute()
+                .path("allOrders")
+                .entityList(Order.class)
+                .hasSize(1);
+    }
+
+}
--- src/test/java/com/wafflecorp/store/controller/ProductControllerTest.java
+++ src/test/java/com/wafflecorp/store/controller/ProductControllerTest.java
@@ -22,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @GraphQlTest(ProductController.class)
 @Import({GraphQlConfig.class})
 class ProductControllerTest {
