b0e62e9e952b93c7c55431e1ccf0a36a542cf24a
Dan Vega <danvega@gmail.com>
2022-04-27 15:37:07 -0400

    cleaning up imports on starter

--- src/main/java/com/wafflecorp/store/StoreApplication.java
+++ src/main/java/com/wafflecorp/store/StoreApplication.java
@@ -1,18 +1,7 @@
 package com.wafflecorp.store;
 
-import com.wafflecorp.store.model.*;
-import com.wafflecorp.store.repository.CustomerRepository;
-import com.wafflecorp.store.repository.OrderRepository;
-import com.wafflecorp.store.repository.ProductRepository;
-import com.wafflecorp.store.repository.ReviewRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 public class StoreApplication {
