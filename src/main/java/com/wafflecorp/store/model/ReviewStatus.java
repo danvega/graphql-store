a97a97ef64af56b213b295d9b2cd4410e0433683
Dan Vega <danvega@gmail.com>
2023-07-27 10:14:50 -0400

    No longer using reviews

--- src/main/java/com/wafflecorp/store/model/ReviewStatus.java
+++
@@ -1,7 +0,0 @@
-package com.wafflecorp.store.model;
-
-public enum ReviewStatus {
-    PENDING,
-    APPROVED,
-    DECLINED
-}
--- src/main/java/com/wafflecorp/store/repository/ReviewRepository.java
+++ src/main/java/com/wafflecorp/store/repository/ReviewRepository.java
@@ -1,6 +1,5 @@
 package com.wafflecorp.store.repository;
 
-import com.wafflecorp.store.model.Review;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewRepository extends CrudRepository<Review,Integer> {
