bd5836a306267ae7383839c25d786c172558c7a3
Dan Vega <danvega@gmail.com>
2023-07-27 06:23:58 -0400

    Adding inspection report

--- src/main/java/com/wafflecorp/store/config/GraphQlConfig.java
+++ src/main/java/com/wafflecorp/store/config/GraphQlConfig.java
@@ -1,18 +1,29 @@
 package com.wafflecorp.store.config;
 
 import graphql.scalars.ExtendedScalars;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @Configuration
 public class GraphQlConfig {
+    
+    private static final Logger log = LoggerFactory.getLogger(GraphQlConfig.class);
 
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
         return wiringBuilder -> wiringBuilder
                 .scalar(ExtendedScalars.Date)
-                .scalar(ExtendedScalars.DateTime);
+                .scalar(ExtendedScalars.DateTime)
+                .scalar(ExtendedScalars.GraphQLBigDecimal);
+    }
+
+    @Bean
+    GraphQlSourceBuilderCustomizer inspectionCustomizer() {
+        return source -> source.inspectSchemaMappings(report -> log.info(report.toString()));
     }
 
 }
