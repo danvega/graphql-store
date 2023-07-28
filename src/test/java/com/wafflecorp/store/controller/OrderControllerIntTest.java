c25ffa4b795cdbf1f1c59d5c865f7a2346288f34
Dan Vega <danvega@gmail.com>
2023-07-28 14:35:59 -0400

    Removing unused import from OrderControllerIntTest

--- src/test/java/com/wafflecorp/store/controller/OrderControllerIntTest.java
+++ src/test/java/com/wafflecorp/store/controller/OrderControllerIntTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.graphql.execution.ErrorType;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
