ee2b588b9b1859aa106a7dec6aef7ae726477b6a
Dan Vega <danvega@gmail.com>
2024-08-19 23:17:45 -0400

    Adding Client Example

---
+++ src/main/java/com/wafflecorp/store/ClientApp.java
@@ -0,0 +1,59 @@
+package com.wafflecorp.store;
+
+import com.wafflecorp.store.product.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
+import org.springframework.graphql.client.HttpSyncGraphQlClient;
+import org.springframework.web.client.RestClient;
+import reactor.core.publisher.Mono;
+
+@Import(RestClientAutoConfiguration.class)
+public class ClientApp implements ApplicationRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientApp.class);
+    private final HttpSyncGraphQlClient client;
+
+    public ClientApp(RestClient.Builder builder) {
+        RestClient restClient = builder.baseUrl("http://localhost:8080/graphql").build();
+        this.client = HttpSyncGraphQlClient.builder(restClient).build();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ClientApp.class).web(WebApplicationType.NONE).run(args);
+    }
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        logger.info("Synchronously Retrieving Products ========================");
+        Product product = client.documentName("product")
+                .variable("id", 1)
+                .retrieveSync("getProduct")
+                .toEntity(Product.class);
+        logger.info("Product {}",product);
+
+
+        logger.info("Asynchronously Retrieving Products ========================");
+        Mono<Product> p2 = client.documentName("product")
+                .variable("id", 2)
+                .retrieve("getProduct")
+                .toEntity(Product.class);
+        Mono<Product> p3 = client.documentName("product")
+                .variable("id", 3)
+                .retrieve("getProduct")
+                .toEntity(Product.class);
+        Mono<Product> p4 = client.documentName("product")
+                .variable("id", 4)
+                .retrieve("getProduct")
+                .toEntity(Product.class);
+
+        Mono.zip(p2,p3, p4).doOnNext(System.out::println).block();
+    }
+}
